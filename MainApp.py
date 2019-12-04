4c08e631e8e328be7019655856d2196528666029
Алексей Пристайко <alex.pristaiko@gmail.com>
2019-12-04 18:24:06 +0300

    Поставил pass на обработку пробела, W, A, D

--- MainApp.py
+++ MainApp.py
@@ -1,6 +1,12 @@
 import  pygame
 
 
+pygame.init()
+size = width, height = 800, 600
+screen = pygame.display.set_mode(size)
+
+
+
 running = True
 while running:
 
@@ -9,8 +15,25 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+         #Выход при нажатии Esc
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+         #Пробел
+        if event.type == pygame.KEYDOWN:
+            if event.unicode == ' ':
+                pass
+         #W
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                pass
+         #A
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                pass
+         #D
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:
+                pass
 
     pygame.display.flip()
