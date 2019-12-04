bed4f94a88adfa7e55dd5536d5c6c789ab7d9b87
Алексей Пристайко <alex.pristaiko@gmail.com>
2019-12-04 18:15:24 +0300

    Выходит на Esc

---
+++ MainApp.py
@@ -0,0 +1,16 @@
+import  pygame
+
+
+running = True
+while running:
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+
+    pygame.display.flip()
