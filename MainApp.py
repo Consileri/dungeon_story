256a20418c2ead81e91ed97faa1590051b3cdcae
AlegMachine <alex.pristaiko@gmail.com>
2019-12-21 18:27:30 +0300

    добавил константы размеров доски

--- MainApp.py
+++ MainApp.py
@@ -1,6 +1,9 @@
 import pygame
 from main import Block, Boss, Mob, Player, Board
 
+COLUMS = 256
+ROWS = 16
+CELL_CIZE = 20
 
 pygame.init()
 
@@ -11,7 +14,7 @@ running = True
 
 block = Block(5, 5)
 cell_size = 80
-board = Board(size[0] // cell_size , size[1] // cell_size, cell_size)
+board = Board(ROWS, COLUMS, CELL_CIZE)
 player = Player(3, 3)
 boss = Boss()
 mob = Mob(5, 6)
