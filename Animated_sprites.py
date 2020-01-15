4718e20f836ee519c7289653f95c5fcc77433aaf
Александр Шахов <kurogiri@yandex.ru>
2020-01-15 18:07:54 +0300

    фикс сидения

--- Animated_sprites.py
+++ Animated_sprites.py
@@ -1,13 +1,27 @@
-import pygame
-from MainApp import load_image
+import pygame, os
 
-MOVE_SPEED = 2
+MOVE_SPEED = 15
 JUMP_POWER = 10
 GRAVITY = 0.35
+tile_width = tile_height = 64
 
 all_sprites = pygame.sprite.Group()
 
+def load_image(name):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print("Can't load image:", name)
+        raise SystemExit(message)
+    image = image.convert_alpha()
+    return image
+
 class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, *groups):
+        super().__init__(*groups)
+        self.count = 0
+
     def move_left(self):
         self.cur_frame = 0
         self.animation = 'walk_left'
@@ -45,19 +59,20 @@ class AnimatedSprite(pygame.sprite.Sprite):
 
     def update(self):
         n = len(self.frames[self.animation])
-        self.cur_frame += 1
-        if self.animation == 'death_right' or self.animation == 'death_left' and self.cur_frame == n:
-            self.kill()
-            return
-        self.cur_frame = self.cur_frame % n
-        self.image = self.frames[self.animation][self.cur_frame]
-
+        if self.count % 10 == 0:
+            self.cur_frame += 1
+            if self.animation == 'death_right' or self.animation == 'death_left' and self.cur_frame == n:
+                self.kill()
+                return
+            self.cur_frame = self.cur_frame % n
+            self.image = self.frames[self.animation][self.cur_frame]
+        self.count += 1
 
 class AnimatedSpriteDemon(AnimatedSprite):
     def __init__(self, x, y):
         super().__init__(all_sprites)
 
-        frames =  self.cut_sheet(load_image('Sprite_sheets/Demon_Boss/attack.png'), 4, 4)
+        frames =  self.cut_sheet(load_image('Sprite_sheets/Demon_Boss/demon_attack.png'), 4, 4)
 
         self.frames = {
             #'bottom': self.frames[:4],
@@ -75,8 +90,8 @@ class AnimatedSpriteDemon(AnimatedSprite):
         self.frames['walk_left'] = frames[12:16]
         self.animation = 'walk_left'
         self.cur_frame = 0
-        self.image = self.frames[self.cur_frame]
-        self.rect = self.rect.move(x, y)
+        self.image = self.frames[self.animation][self.cur_frame]
+        self.rect = self.rect.move(x * tile_width, y * tile_width)
 
 
 class AnimatedSpriteKnight(AnimatedSprite):
@@ -104,29 +119,32 @@ class AnimatedSpriteKnight(AnimatedSprite):
         self.frames['idle_left'] = frames
         self.animation = 'walk_left'
         self.cur_frame = 0
-        self.image = self.frames[self.cur_frame]
-        self.rect = self.rect.move(x, y)
+        self.image = self.frames[self.animation][self.cur_frame]
+        self.rect = self.rect.move(x * tile_width, y * tile_width)
 
 
 class AnimatedSpriteMH(AnimatedSprite):
     def __init__(self, x, y):
         super().__init__(all_sprites)
 
-        frames = self.cut_sheet(load_image('Sprite_sheets/Main_hero/main_hero.png'), 7, 16)
+        frames = self.cut_sheet(load_image('Sprite_sheets/Main_hero/main_hero.png'), 8, 16)
 
         self.frames = {
             'idle_right' : frames[0:4],
             'idle_left' : list(map(lambda surface: pygame.transform.flip(surface, True, False), frames[0:4])),
-            'walk_right' : frames[7:14],
-            'walk_left' : list(map(lambda surface: pygame.transform.flip(surface, True, False), frames[7:14])),
+            'walk_right' : frames[9:16],
+            'walk_left' : list(map(lambda surface: pygame.transform.flip(surface, True, False), frames[9:16])),
             'attack_right' : frames[35:47],
             'attack_left' : list(map(lambda surface: pygame.transform.flip(surface, True, False), frames[35:47]))
         }
-        self.animation = 'walk_left'
+        self.animation = 'walk_right'
         self.cur_frame = 0
-        self.image = self.frames[self.cur_frame]
-        self.rect = self.rect.move(x, y)
-    def update(self, left, right, up, platforms):
+        self.onGround = True
+        self.xvel = 0
+        self.yvel = 0
+        self.image = self.frames[self.animation][self.cur_frame]
+        self.rect = self.rect.move(x * tile_width, y * tile_width)
+    def update2(self, left, right, up, platforms):
         if left:
             self.xvel = -MOVE_SPEED  # Лево = x- n
 
@@ -190,6 +208,5 @@ class AnimatedSpriteWizard(AnimatedSprite):
         self.frames['attack_left'] = list(map(lambda surface: pygame.transform.flip(surface, True, False), frames))
         self.animation = 'walk_left'
         self.cur_frame = 0
-        self.image = self.frames[self.cur_frame]
-        self.rect = self.rect.move(x, y)
-
+        self.image = self.frames[self.animation][self.cur_frame]
+        self.rect = self.rect.move(x * tile_width, y * tile_width)
